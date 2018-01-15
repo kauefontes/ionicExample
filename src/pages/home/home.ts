f5c06c0f63b9860e8631f5ebe0e2726b29aac22c
Kaue Pereira <kaue@MacBook-Pro-de-Kaue.local>
2018-01-14 21:49:36 -0400

    some shit changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public photos: any;
+  public base64Image: string;
 
+  constructor(public navCtrl: NavController,
+              private camera: Camera,
+              private alertCtrl: AlertController) {}
+
+  ngOnInit() {
+    this.photos = [];
   }
+  deletePhoto(index){
+    let confirm = this.alertCtrl.create({
+      title: 'Tem certeza que deseja apagar a imagem?',
+      message: 'Esta ação não poderá ser desfeita!',
+      buttons: [
+        {
+          text: 'Não',
+          handler: () => {
+            console.log("Deletion cancelled!");
+          }
+        }, {
+          text: 'Sim',
+          handler: () => {
+            this.photos.splice(index, 1);
+          } 
+        }
+      ]
+    });
+    confirm.present();
+  };
+
+  takePhoto(){
+    const options : CameraOptions = {
+      quality: 50, //picture quality
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((ImageData) => {
+      this.base64Image = "data:image/jpeg;base64," + ImageData;
+      this.photos.push(this.base64Image);
+      this.photos.reverse();
+    }, (err) => {
+      console.log(err);
+    });
+  };
 
 }
